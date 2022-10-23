ce391de32caaf06c4b57a36b7bfaa0fa129405b6
Denys Vekeryk <den.vekeryk@gmail.com>
2022-10-23 23:48:25 +0300

    Removed hibernate property

--- src/main/java/org/booking/config/SpringConfig.java
+++ src/main/java/org/booking/config/SpringConfig.java
@@ -77,7 +77,6 @@ public class SpringConfig implements WebMvcConfigurer {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-//        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
         return properties;
     }
 
