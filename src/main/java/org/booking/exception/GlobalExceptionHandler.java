63f148de1d0a96e0679d5d6260a535f2a19506c0
Denys Vekeryk <den.vekeryk@gmail.com>
2022-10-23 23:47:31 +0300

    Added error handling

---
+++ src/main/java/org/booking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package org.booking.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    public ModelAndView numberFormatExceptionHandler(HttpServletRequest request, ResponseStatusException exception) {
+        return getModelAndView(request, exception.getStatus(), exception);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    public ModelAndView numberFormatExceptionHandler(HttpServletRequest request, NumberFormatException exception) {
+        return getModelAndView(request, HttpStatus.BAD_REQUEST, exception);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    public ModelAndView entityNotFoundExceptionHandler(HttpServletRequest request, EntityNotFoundException exception) {
+        return getModelAndView(request, HttpStatus.NOT_FOUND, exception);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    public ModelAndView accessDeniedExceptionHandler(HttpServletRequest request, AccessDeniedException exception) {
+        return getModelAndView(request, HttpStatus.FORBIDDEN, exception);
+    }
+
+    private ModelAndView getModelAndView(HttpServletRequest request, HttpStatus httpStatus, Exception exception) {
+        log.error("Exception {} raised = {} :: URL = {}", exception.getClass(), exception.getMessage(), request.getRequestURL());
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("code", httpStatus.value() + " / " + httpStatus.getReasonPhrase());
+        modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
+    }
+}
